243ba37f3d6524a633147ba4bc62f49d1e418cc3
Antônio Diego <diego-luz@live.com>
2019-07-18 21:48:46 -0300

    Tentei fazer melhorias.

--- main.cpp
+++ main.cpp
@@ -20,13 +20,35 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    cout << "Calculadora de transf. PP\n";
     while (true) {
-        cout << "\nDigite o valor a transferir desejado:";
+        cout << "Escolha uma opção\n";
+        cout << "1. Calcular aplicação de taxas";
+        cout << "2. Calcular valor antes de aplicação de taxas";
+        int opc;
+        cin>>opc;
+        double valorTa;
+        double valorT;
         double valorD;
-        cin>>valorD;
-        double ve = (100 * valorD + 60) / 95.21;
-        cout << "\nO valor enviado para que chegue R$ "<<valorD<<" deverá ser:" << ve;
+        double ve;
+        switch (opc) {
+            case 1:
+                cout << "\nDigite o valor a transferir desejado:";
+                cin>>valorT;
+                valorTa = valorT - 0.6 - (valorT * (4.79 / 100));
+                cout << "\nValor com taxas: " << valorTa;
+                break;
+            case 2:
+                cout << "\nDigite o valor com taxas desejado:";
+                valorD;
+                cin>>valorD;
+                ve = (100 * valorD + 60) / 95.21;
+                cout << "\nO valor enviado para que chegue R$ " << valorD << " deverá ser:" << ve;
+                break;
+            default:
+                cout << "Opção inválida!";
+                break;
+        }
     }
 
     return 0;
