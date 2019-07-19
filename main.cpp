e7561bbd731a9bda37f6667a009ad9460d7509e8
Antônio Diego <diego-luz@live.com>
2019-07-18 21:55:08 -0300

    Tentei organizar quebras de linhas e melhorar info. resultado.

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ int main(int argc, char** argv) {
     cout << "Calculadora de transf. PP\n";
     while (true) {
         cout << "Escolha uma opção\n";
-        cout << "1. Calcular aplicação de taxas";
-        cout << "2. Calcular valor antes de aplicação de taxas";
+        cout << "1. Calcular aplicação de taxas\n";
+        cout << "2. Calcular valor antes de aplicação de taxas\n";
         int opc;
         cin>>opc;
         double valorTa;
@@ -36,17 +36,17 @@ int main(int argc, char** argv) {
                 cout << "\nDigite o valor a transferir desejado:";
                 cin>>valorT;
                 valorTa = valorT - 0.6 - (valorT * (4.79 / 100));
-                cout << "\nValor com taxas: " << valorTa;
+                cout << "\nValor R$ "<<valorT<<" com taxas: " << valorTa<<"\n";
                 break;
             case 2:
                 cout << "\nDigite o valor com taxas desejado:";
                 valorD;
                 cin>>valorD;
                 ve = (100 * valorD + 60) / 95.21;
-                cout << "\nO valor enviado para que chegue R$ " << valorD << " deverá ser:" << ve;
+                cout << "\nO valor enviado para que chegue R$ " << valorD << " deverá ser:" << ve<<"\n";
                 break;
             default:
-                cout << "Opção inválida!";
+                cout << "Opção inválida!\n";
                 break;
         }
     }
